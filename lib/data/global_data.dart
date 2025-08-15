3e46b4e63912fac33b22b027851f6feb7f9f3c42
Khanh <thedarkspiritaway@gmail.com>
2025-08-15 16:22:03 +0700

    firebase hosting init

--- lib/data/global_data.dart
+++ lib/data/global_data.dart
@@ -1,8 +1,5 @@
 abstract class GlobalData {
-  // static String baseUrl = "https://medlab-backend.onrender.com";
-  // static String socketUrl = "wss://medlab-backend.onrender.com";
-  static String baseUrl = "http://localhost:3000";
-  static String socketUrl = "ws://localhost:3000";
+  static String baseUrl = "https://go-nqk-entry-be.onrender.com";
+  // static String baseUrl = "http://localhost:3000";
   // static String baseUrl = "http://192.168.10.8:3000";
-  // static String socketUrl = "ws://192.168.10.8:3000";
 }
